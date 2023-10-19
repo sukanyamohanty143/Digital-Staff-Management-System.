6891f761de17109781023d1f795c247e8de5e02e
Ujala <ujala21@navgurukul.com>
2023-10-19 19:37:53 +0530

    completed UI and Fetching DummyData

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import Home from "./Components/Pages/Home";
 import Registration from "./Components/Pages/Registration";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import SupervisorPage from "./Components/Pages/Supervisor/SuppervisorPage"
-
+// import SupervisorPage from "./Components/Pages/Supervisor/SuppervisorPage"
+import AdminPage from "./Components/Pages/Admin/admin"
 function App(){
 
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/registration" element={<Registration />} />
-            </Routes>
-        </Router>
+        <>
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/registration" element={<Registration />} />
+                </Routes>
+            </Router>
+            <AdminPage/>
+            </>
+        </>
+
     );
 }
 
---
+++ src/Components/Pages/Admin/admin.js
@@ -0,0 +1,61 @@
+import React,{useState,useEffect} from "react"
+import {TableContainer,Table,TableHead,TableCell,TableBody,TableRow,Paper,Typography} from "@mui/material";
+const dummyData = [
+    {
+      id: 1,
+      name: "John Doe",
+      mobileNumber: "123-456-7890",
+      designation: "Software Engineer",
+      gender: "Male",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      mobileNumber: "987-654-3210",
+      designation: "Product Manager",
+      gender: "Female",
+    },
+    {
+      id: 3,
+      name: "Bob Johnson",
+      mobileNumber: "555-123-4567",
+      designation: "UI/UX Designer",
+      gender: "Male",
+    },
+  ];
+const AdminPage=()=>{
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+        setUsers(dummyData);
+    },[]);
+    return(
+        <>
+            <Typography variant="h4" align="center" color="primary">Admin Page</Typography>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Mobile number</TableCell>
+                            <TableCell>Designation</TableCell>
+                            <TableCell>Gender</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users.map((user)=>(
+                            <TableRow key={user.id}>
+                                <TableCell>{user.id}</TableCell>
+                                <TableCell>{user.name}</TableCell>
+                                <TableCell>{user.mobileNumber}</TableCell>
+                                <TableCell>{user.designation}</TableCell>
+                                <TableCell>{user.gender}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
+export default AdminPage;
