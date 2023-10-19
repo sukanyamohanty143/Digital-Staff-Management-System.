33681a22b6c3b6e3c3257a28fb97af7f16b36149
khushboo kumari <khushboo20@navgurukul.org>
2023-10-19 14:29:42 +0530

    created componet of supervisor page and createt UI

---
+++ src/Components/Pages/Supervisor/SuppervisorPage.js
@@ -0,0 +1,15 @@
+
+import SreachBar from "./SreachBar";
+import TableData from "./TableData"
+
+function SupervisorPage() {
+    return (
+        <>
+       
+        <SreachBar/>
+        <TableData/>
+    
+        </>
+    )
+}
+export default SupervisorPage;
