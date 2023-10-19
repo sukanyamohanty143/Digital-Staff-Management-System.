3ef51cc6830350d6cc5840349d09cc509109aa15
khushboo kumari <khushboo20@navgurukul.org>
2023-10-19 22:29:29 +0530

    fitch data from json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
-import "./App.css";
-import React from 'react';
-import Home from "./Components/Pages/Home";
-import Staff from "./Components/Pages/Staff"
-import Registration from "./Components/Pages/Registration";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import "./App.css";
+// import React from 'react';
+// import Home from "./Components/Pages/Home";
+// // import Staff from "./Components/Pages/Staff"
+// import Registration from "./Components/Pages/Registration";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import SupervisorPage from "./Components/Pages/Supervisor/SuppervisorPage"
 
@@ -12,16 +12,23 @@ function App(){
 
     return (
       <>
-        <Router>
+      <div className="App">
+        
+        
+        {/* <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/registration" element={<Registration />} />
                 
             </Routes>
         </Router>
-        <Staff/>
-        <br></br>
+        {/* <Staff/> */}
+        {/* <br></br>  */}
         <SupervisorPage/>
+       
+        </div>
+
+       
         </>
 
     ); 
--- src/Components/Pages/Supervisor/SuppervisorPage.js
+++ src/Components/Pages/Supervisor/SuppervisorPage.js
@@ -1,14 +1,41 @@
 
+// import { useState } from "react";
+import { useEffect, useState } from "react";
 import SreachBar from "./SreachBar";
+
 import TableData from "./TableData"
+// import { useState,useEffect} from "react"//;
+
+import employeesData from './dataserver.json';
 
 function SupervisorPage() {
+
+    const employees = employeesData.employees;
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        setData(employees)
+    },[employees])
+
+    // console.log(employees,"data")
+    console.log(data, "data")
+
+    // console.log(employees,"data")
+
+    // const [data,setData]=useState([])
+
+    // setData(employees)
+    // console.log(data,"kkkkk")
+
     return (
         <>
-       
-        <SreachBar/>
-        <TableData/>
-            
+           
+
+            <SreachBar/>
+            <TableData employees={data} />
+
+
         </>
     )
 }
