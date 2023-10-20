0199926478d3d6320efd28b1c3860deb5c7a21e8
khushboo kumari <khushboo20@navgurukul.org>
2023-10-21 00:10:46 +0530

    PDF functionality Added

---
+++ src/Components/Pages/Supervisor/PdfCom.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import jsPDF from 'jspdf';
+
+function PdfCom({ filterArr }) {
+
+    const HandlerPDF = () => {
+        const dic = new jsPDF();
+
+        filterArr.forEach((item, index) => {
+            const text = `${item.FirstName} LastName : ${item.LastName} Mobile Number ${item.Mobile} designation: ${item.designation} Gender: ${item.Gender} attendance: ${item.attendance} Date: ${item.date}`;
+            dic.text(text, 3, 10 + index * 10);
+        });
+
+        dic.save("khushboo.pdf");
+    }
+
+    return (
+        <>
+            <Button variant="contained" onClick={HandlerPDF}>Convert into PDF</Button>
+        </>
+    )
+}
+
+export default PdfCom;
