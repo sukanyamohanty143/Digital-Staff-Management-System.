ea1a12f9be7be6f364584d8f094751ac4735c116
Ujala <ujala21@navgurukul.com>
2023-10-22 00:16:33 +0530

    Using mui form page in center

--- src/Components/Pages/AdminPage/AddButton.js
+++ src/Components/Pages/AdminPage/AddButton.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import {List,ListItem,Button,TextField} from '@mui/material';
 import "./style.css"
 const UserForm = ({ onAddUser }) => {
     const [id, setId] = useState('');
@@ -25,36 +25,48 @@ const UserForm = ({ onAddUser }) => {
     };
     return (
         <>
-              <Box>
-                    <TextField
-                        label="id"
-                        value={id}
-                        onChange={(e) => setId(e.target.value)}
-                    />
-                    <TextField
-                        label="Name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <TextField
-                        label="Mobile Number"
-                        value={mobileNumber}
-                        onChange={(e) => setMobileNumber(e.target.value)}
-                    />
-                    <TextField
-                        label="Designation"
-                        value={designation}
-                        onChange={(e) => setDesignation(e.target.value)}
-                    />
-                    <TextField
-                        label="Gender"
-                        value={gender}
-                        onChange={(e) => setGender(e.target.value)}
-                    />
-              </Box>
-              <Button variant="contained" onClick={addUser}>Add Form</Button>
+            <List>
+                <ListItem>
+                  <TextField
+                    label="ID"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                  />
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    label="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    label="Mobile Number"
+                    value={mobileNumber}
+                    onChange={(e) => setMobileNumber(e.target.value)}
+                  />
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    label="Designation"
+                    value={designation}
+                    onChange={(e) => setDesignation(e.target.value)}
+                  />
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    label="Gender"
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  />
+                </ListItem>
+                <ListItem>
+                    <Button variant="contained" onClick={addUser}>Add Form</Button>
+                </ListItem>
+            </List>
+            {/* <Button variant="contained" onClick={addUser}>Add Form</Button> */}
         </>
   );
 };
-
 export default UserForm;
