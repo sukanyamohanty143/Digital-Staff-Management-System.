3e453ebc5e34d4d4772c8ea07e425a976d6653cf
khushboo kumari <khushboo20@navgurukul.org>
2023-10-23 09:58:52 +0530

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import "./App.css";
 import React from 'react';
-// import Home from "./Components/Pages/Home";
-// import Registration from "./Components/Pages/Registration";
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./Components/Pages/Home";
+import Registration from "./Components/Pages/Registration";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Staff from "./Components/Pages/Staff";
-import AdminPage from "./Components/Pages/AdminPage/admin";
-// import SupervisorPage from "./Components/Pages/Supervisor/SuppervisorPage";
+// import AdminPage from "./Components/Pages/AdminPage/admin";
+import SupervisorPage from "./Components/Pages/Supervisor/SuppervisorPage";
 function App(){
     return (
         <>
-            {/* <Router>
+             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/registration" element={<Registration />} />
                 </Routes>
-            </Router>
-            <Staff/> */}
-            <AdminPage/>
-            {/* <SupervisorPage/> */}
+            </Router> 
+            {/* <Staff/> */} 
+            {/* <AdminPage/> */}
+            <SupervisorPage/>
             
         </>
     )
