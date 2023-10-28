578997058b00dd0dd2547f0f61f6b4f45762da6f
khushboo <khushboo20@navgurukul.org>
2023-10-28 21:26:25 +0530

    Table UI

--- src/Components/Pages/SupervisorTracker/Table.js
+++ src/Components/Pages/SupervisorTracker/Table.js
@@ -1,8 +1,61 @@
-function Table(){
-    return(
+import {
+    Card,
+    CardContent,
+    Grid,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+
+} from '@material-ui/core';
+function TableData({ data }) {
+
+    console.log(data, "filrt")
+
+    return (
+
         <>
-        table
+            
+                <Card container justifyContent="center" >
+                    <CardContent>
+                        <Grid
+                            align="center"
+                            container
+                            direction="column"
+                            justify="center"
+                            spacing={0}
+                        >
+                            <Grid item>
+                                <TableContainer>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow >
+                                                <TableCell style={{ fontSize: "20px",textAlign:"center" }}>FirstName</TableCell>
+                                                <TableCell style={{ fontSize: "20px",textAlign:"center" }}>LastName</TableCell>
+                                                <TableCell style={{ fontSize: "20px",textAlign:"center" }}>Mobile</TableCell>
+
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {data.map((item) => (
+                                                <TableRow key={item.id}>
+                                                    <TableCell style={{ fontSize: "12px",textAlign:"center" }}>{item.name}</TableCell>
+                                                    <TableCell style={{ fontSize: "12px",textAlign:"center" }}>{item.lastame}</TableCell>
+                                                    {/* <TableCell style={{ fontSize: "20px" }}>{item.Mobile}</TableCell> */}
+
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                </Card>
+            
         </>
     )
 }
-export default Table
+export default TableData;
