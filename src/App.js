7d7b3791de83d6ca75b55c81c61d711aa65151fb
laxmi <laxmiyadav21@navgurukul.org>
2023-11-13 12:20:40 +0530

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import "./App.css";
-import React from 'react';
-import Home from "./Components/Pages/Home";
-import Registration from "./Components/Pages/Registration";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from "./Components/Pages/Footer";
-// import SearchBar from "./Components/Pages/SupervisorTracker/SearchBar";
-import Staff from "./Components/Pages/Staff";
-import Profile from "./Components/Pages/Profile";
-// import Admin from "./Components/Pages/AdminPage/admin";
-import Outer from "./Components/Pages/Outer";
-import Table from"./Components/Pages/Table";
+
+import Header from './Components/Pages/Header';
+import Routing from "./Components/Routing/Routing";
+import Footer from './Components/Pages/Footer'
 function App() {
     return (
         <>
-
-
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/registration" element={<Registration />} />
-
-                </Routes>
-            </Router>
-            <Staff/>
-
-<Profile/>
-
-            {/* <SearchBar/> */}
-{/* <Admin/> */}
-            <Footer />
-            <Outer/>
-            <Table/>
-
-
+            <Header/>
+            <Routing/>
+            <Footer/>
             
         </>
     )
--- src/Components/Routing/Routing.js
+++ src/Components/Routing/Routing.js
@@ -4,42 +4,28 @@ import Staff from '../Pages/Staff'
 import Home from '../Pages/Home'
 import Registration from '../Pages/Registration'
 import AdminPage from '../Pages/AdminPage/admin'
-import SupervisorPage from '../Pages/SupervisorTracker/SupervisorPage'
+import Supervisor from '../Pages/SupervisorComponent/Supervisor'
+import Outer from '../Pages/Outer';
+import Profile from '../Pages/Profile'
+import EmployeeTable from '../Pages/Table'
 
 function Routing() {
 
 
   return (
     <BrowserRouter>
-      {/* <NavigationLogic role={role} /> */}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/staff" element={<Staff />} />
         <Route path="/admin" element={<AdminPage />} />
-        <Route path="/supervisor" element={<SupervisorPage />} />
+        <Route path="/supervisor" element={<Supervisor />} />
+        <Route path="/outer" element={<Outer/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/table" element={<EmployeeTable />} />
       </Routes>
     </BrowserRouter>
   );
 }
-export default Routing
-
-// function NavigationLogic({ role }) {
-
-//   const navigate = useNavigate();
-//     console.log(role,"user role")
-//   if (role === 'staff') {
-//     navigate('/staff');
-//   } else if (role === 'admin') {
-//     navigate('/admin');
-//   } else if (role === 'supervisor') {
-//     navigate('/supervisor');
-//   } else {    
-//     navigate('/');
-//   }
-
-//   return null; 
-// }
-
-// export default Routing;
+export default Routing;
